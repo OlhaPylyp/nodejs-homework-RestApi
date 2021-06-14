cbd37ac2098bec91a20196a545a82a62f119d1b4
Olha Pylypova <olhasve@gmail.com>
2021-06-14 23:06:47 +0300

    change intomap

--- model/index.js
+++ model/index.js
@@ -77,7 +77,7 @@ const changeContact = async (contactId, name, email, phone) => {
   try {
     const listContact = await fs.readFile(contactsPath, 'utf8')
     const contact = JSON.parse(listContact)
-    contact.map(cont => {
+    contact.forEach(cont => {
       if (cont.id.toString() === contactId) {
         if (name) { cont.name = name }
         if (email) { cont.email = email }

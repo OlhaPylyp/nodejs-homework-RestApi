9d55bc178100197cd2153005a40547c5ba20212b
OlhaPylyp <olhasvet@gmail.com>
2022-06-15 22:17:48 +0300

    dddd

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
-app.use("/api/contacts", contactsRouter);
+app.use("/api/cont", contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });

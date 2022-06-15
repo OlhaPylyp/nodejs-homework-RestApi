37cdf12d5a4b0bbd8b269f5373b97bc2ebc43814
OlhaPylyp <olhasvet@gmail.com>
2022-06-15 21:12:46 +0300

    correct mdel

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
-app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts4", contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });

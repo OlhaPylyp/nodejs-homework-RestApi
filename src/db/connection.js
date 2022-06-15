0a9d7bfba33c14aceef2cf5a25d9a5b819a3dd6b
OlhaPylyp <olhasvet@gmail.com>
2022-06-15 21:26:44 +0300

    fix connecction

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,16 +1,25 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectMongo = async () => {
   try {
-    return (mongoose.connect(process.env.DB_HOST, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }),
-    console.log('Database connection successful'))
+    return (
+      mongoose.connect(
+        // process.env.DB_HOST,
+        "mongodb+srv://olha:olhasvet@cluster0.tm34v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useFindAndModify: false,
+          useCreateIndex: true,
+        }
+      ),
+      console.log("Database connection successful")
+    );
   } catch (err) {
-    console.log(`Error in DB connection ${err}`)
-    process.exit(1)
+    console.log(`Error in DB connection ${err}`);
+    process.exit(1);
   }
-}
+};
 
-module.exports = { connectMongo }
+module.exports = { connectMongo };
